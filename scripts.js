d8edd95fadee663c07d49e1e65477dba8a52abbe
unknown <16tsmith55@gmail.com>
2019-09-29 17:58:24 -0600

    add json

--- scripts.js
+++ scripts.js
@@ -1,26 +1,6 @@
-data = {
-  title: "Subject of Example Quiz : AB2340",
-  author: "Jane Doe",
-  questions: [
-    {
-      ask: "What is the answer to this question?",
-      options: ["Carl", "Eric", "Adam", "Jose"],
-      imagesrc: "https://place-hold.it/200"
-    },
-    {
-      ask: "What is the answer to other question?",
-      options: ["Carl", "Eric", "Adam", "Jose"],
-      imagesrc: "https://place-hold.it/200"
-    },
-    {
-      ask: "How about this question?",
-      options: ["Carl", "Eric", "Adam", "Jose"],
-      imagesrc: "https://place-hold.it/200"
-    },
-    {
-      ask: "And this last one?",
-      options: ["Carl", "Eric", "Adam", "Jose"],
-      imagesrc: "https://place-hold.it/200"
-    }
-  ]
-};
+$.getJSON("data/questions.json", function(json) {
+  console.log(json);
+});
+
+var title = document.getElementsByClassName("title")[0];
+var author = document.getElementsByClassName("author")[0];
