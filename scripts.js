66fb85ac7e2bc319d267cccce245583d7a29ab51
unknown <16tsmith55@gmail.com>
2019-09-29 18:07:50 -0600

    author json

--- scripts.js
+++ scripts.js
@@ -3,4 +3,6 @@ var author = document.getElementsByClassName("author")[0];
 
 $.getJSON("data/questions.json", function(json) {
   title.innerHTML = json.title;
+  author.innerHTML = "By" + json.author;
+  console.log(json);
 });
