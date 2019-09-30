788d55c053e13bee5d6317eb0b1e033abefe68e2
Subtora <subtora@github.com>
2019-09-30 11:05:04 -0600

    add index

--- scripts.js
+++ scripts.js
@@ -1,7 +1,18 @@
 var title = document.getElementsByClassName("title")[0];
 var author = document.getElementsByClassName("author")[0];
 var ask = document.getElementsByClassName("ask")[0];
+var picknext = document.getElementById('picknext');
+var pickprev = document.getElementById('pickprev');
 var data;
+
 $.getJSON("data/questions.json", function(json) {
   data = json;
 });
+
+function setData(index){
+  title.innerHTML = data.title;
+  author.innerHTML = "By " + data.author;
+  ask.innerHTML = data.questions[index].ask;
+}
+
+setData(0);
