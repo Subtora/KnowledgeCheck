15f58b0e0e8687d07ad914ef26aee681bae47502
unknown <16tsmith55@gmail.com>
2019-09-29 18:06:13 -0600

    fix

--- scripts.js
+++ scripts.js
@@ -2,5 +2,5 @@ var title = document.getElementsByClassName("title")[0];
 var author = document.getElementsByClassName("author")[0];
 
 $.getJSON("data/questions.json", function(json) {
-  title.innerHTML = title;
+  title.innerHTML = json.title;
 });
