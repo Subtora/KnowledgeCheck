d53cdc0984d8786667b261b69503588b1013f10b
unknown <16tsmith55@gmail.com>
2019-09-29 18:05:33 -0600

    set title test

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-$.getJSON("data/questions.json", function(json) {
-  console.log(json);
-});
-
 var title = document.getElementsByClassName("title")[0];
 var author = document.getElementsByClassName("author")[0];
+
+$.getJSON("data/questions.json", function(json) {
+  title.innerHTML = title;
+});
